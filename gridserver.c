d86879016c2eba551a2491fb3e64a246a5e29bca
Zombieranke <abcdefghijklmnop@qrs>
2015-05-22 11:23:11 +0200

    added libraries tog gridserver

--- gridserver.c
+++ gridserver.c
@@ -5,9 +5,19 @@
  *      Author: bomwald
  */
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#define KEY 13371337
+#define PERM 0660
 
 int main()
 {
+	int msgid;
+
+	if( (msgid = msgget(KEY,PERM|IPC_CREAT|IPC_EXCL ))==-1 ){
+	/* error handling */
+	}
+
 	return 0;
 }
-
