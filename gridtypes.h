8decc9239e06587f195c38f3ceaa9bf9cfbd845a
zeldafan42 <zeldafan42@gmail.com>
2015-05-22 12:05:50 +0200

    gridtypes.h for shared names for pipes msg struct etc.

---
+++ gridtypes.h
@@ -0,0 +1,16 @@
+/*
+ * gridtypes.h
+ *
+ *  Created on: May 22, 2015
+ *      Author: bomwald
+ */
+
+#ifndef GRIDTYPES_H_
+#define GRIDTYPES_H_
+
+#define PIPE_NAME "display.pipe"
+#define KEY 13371337
+#define PERM 0660
+
+
+#endif /* GRIDTYPES_H_ */
