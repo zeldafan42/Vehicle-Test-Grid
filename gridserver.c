55675e8dac57f0baa5fe80a2d23ab5494a7704f0
Zombieranke <abcdefghijklmnop@qrs>
2015-05-22 13:09:20 +0200

    finished getStartingPosition(...) in gridserver.c and sending the starting coordinates to the client should work approximately like that

--- gridserver.c
+++ gridserver.c
@@ -11,40 +11,86 @@
 #include <sys/msg.h>
 
 
-void getStartingPosition(Message_handshake* serverHandshake);
-void makeMove(Client* client, char action);
+int getStartingPosition(Message_handshake* serverHandshake, char* field, int fieldX, int fieldY);
+void makeMove(Client* client, char action, char* field, int fieldX, int fieldY);
 
 int main()
 {
 	int msgid;
-	int currentClientNumber;
-	Client clients [26];
-
-
-	Message_move clientMessage;
-	Message_handshake serverHandshake;
 
 
 
-	if( (msgid = msgget(KEY,PERM|IPC_CREAT|IPC_EXCL ))==-1 ){
+
+	if( (msgid = msgget(KEY,PERM|IPC_CREAT|IPC_EXCL ))==-1 )
+	{
 	/* error handling */
 	}
 
-	while(msgrcv(msgid,&clientMessage,sizeof(msg)-sizeof(long),-25, 0) == -1)
-	{
-		currentClientNumber = clientMessage->msgType;
-		clients[currentClientNumber]->pid = clientMessage->pid;
 
-		if(clientMessage->action == '0')
-		{
-			serverHandshake->msgType = (clientMessage->msgType + 26);
-			getStartingPosition(&serverHandshake);
-			msgsnd(msgid,)
-		}
-
-	}
 
 	return 0;
 }
 
 
+void runningServer(int msgid, int fieldX, int fieldY)
+{
+	Message_move setup1;
+	Message_handshake setup2;
+
+	Message_move* clientMessage = &setup1;
+	Message_handshake* serverHandshake = &setup2;
+
+	int currentClientNumber;
+	Client clients [26];
+
+	char* field = malloc(sizeof(char)*(fieldX+2)*(fieldY+2));
+
+
+
+	while(msgrcv(msgid,&clientMessage,sizeof(Message_move)-sizeof(long),-25, 0) == -1)
+		{
+			currentClientNumber = clientMessage->msgType;
+			clients[currentClientNumber]->pid = clientMessage->pid;
+
+			if(clientMessage->action == '0')
+			{
+				serverHandshake->msgType = (clientMessage->msgType + 26);
+				if(getStartingPosition(serverHandshake) == -1)
+				{
+					/* send SIGTERM because there is no space */
+				}
+				else
+				{
+					msgsnd(msgid, serverHandshake, sizeof(Message_handshake)-sizeof(long), serverHandshake->msgType, PERM);
+				}
+			}
+
+		}
+}
+
+
+void makeMove(Client* client, char action, char* field, int fieldX, int fieldY)
+{
+
+}
+
+int getStartingPosition(Message_handshake* serverHandshake, char* field, int fieldX, int fieldY)
+{
+	int x = 0;
+	int y = 0;
+
+	for(x=1; x < (fieldX-1); x++)
+	{
+		for(y=1; y < (fieldY-1); y++)
+		{
+			if(field[y*fieldX + x] == ' ')
+			{
+				serverHandshake->startX = x;
+				serverHandshake->startY = y;
+				return 0;
+			}
+		}
+	}
+	return -1;
+
+}
